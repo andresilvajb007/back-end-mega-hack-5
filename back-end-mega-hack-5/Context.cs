559cfd21f486a5e84c24aab06be7bae1aa33d513
andre silva <andre.silvajb@gmailcom>
2020-11-14 10:59:46 -0300

    initial

---
+++ back-end-mega-hack-5/Context.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace back_end_mega_hack_5
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions dbContextOptions)
+            :base(dbContextOptions)
+        {
+        }
+
+
+        //public DbSet<Incentivador> Incentivador { get; set; }        
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Incentivador>(ConfigureIncentivador);
+ 
+        //}
+
+        //private void ConfigurePagamentoIncentivadorPIX(EntityTypeBuilder<PagamentoIncentivadorPIX> obj)
+        //{
+        //    obj.HasKey(x => x.Id);
+
+     
+        //}
+
+    }
+}
---
+++ back-end-mega-hack-5/Startup.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace back_end_mega_hack_5
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            // Filter out ASP.NET Core infrastructre logs that are Information and below
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo
+            .Console()
+            .CreateLogger();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<Context>(options => options.UseNpgsql("Host=ec2-54-156-85-145.compute-1.amazonaws.com; Port=5432;Database=d9b9nmidcafjsr;Username=jqdsyavlscywym;Password=3621ae6dd272343e6222ad37b8738f22281bdccce44ada24315c22fa7aa73977;SSL Mode=Require; Trust Server Certificate=true;"));
+
+
+            services.AddSwaggerGen(x => x.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo {Version = "v1", Title = "Web API" }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSerilogRequestLogging();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();                
+            }
+            
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI(v1)");
+            }
+             );
+
+            //app.UseHttpsRedirection();
+
+            app.UseCors(c =>
+            {
+                c.AllowAnyHeader();
+                c.AllowAnyMethod();
+                c.AllowAnyOrigin();
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
